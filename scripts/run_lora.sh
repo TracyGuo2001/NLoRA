0a1a02261334bec3043bf447ea47a1e588e7445d
TracyGuo2001 <751591713@qq.com>
2025-02-21 16:12:18 +0800

    code_v1

---
+++ scripts/run_lora.sh
@@ -0,0 +1,31 @@
+BASE_MODEL="meta-llama/Llama-2-7b-hf"
+OUTPUT_PATH="Output"
+DATA_PATH="meta-math/MetaMathQA"
+
+# batch size = per_device_train_batch_size * gradient_accumulation_steps * num_gpus = 128
+deepspeed --master_port=16971 --include=localhost:0,1,2,3 nlora.py \
+    --deepspeed configs/ds_config_zero2_no_offload.json \
+    --model_name_or_path $BASE_MODEL \
+    --use_lora True \
+    --init_lora_weights True \
+    --target_modules "q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj" \
+    --lora_rank 128 \
+    --lora_alpha 128 \
+    --lora_dropout 0 \
+    --data_path $DATA_PATH \
+    --dataset_field query response \
+    --dataset_split "train[:100000]"\
+    --output_dir $OUTPUT_PATH \
+    --num_train_epochs 1 \
+    --model_max_length 512 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 100 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --logging_steps 1 \
+    --lr_scheduler_type "cosine" \
+    --merge True \
+    --warmup_ratio 0.03 \
